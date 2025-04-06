377f99a1ec8a7bf0b6913ebeb2b442abd6227aaf
grrimgrriefer <168567347+grrimgrriefer@users.noreply.github.com>
2025-04-07 00:16:00 +0200

    working connection between Rust backend and hismith via intiface & bluetooth

--- rust-website/src/main.rs
+++ rust-website/src/main.rs
@@ -1,12 +1,22 @@
 use actix_web::{App, HttpServer};
 use rust_website::routes;
+use rust_website::buttplug;
 use env_logger::Env;
 use dotenv::dotenv;
+use tokio::task;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
     env_logger::init_from_env(Env::default().default_filter_or("info"));
+
+    // Spawn the `do_the_thing` function in the background
+    task::spawn(async {
+        if let Err(e) = buttplug::do_the_thing().await {
+            eprintln!("Error running do_the_thing: {}", e);
+        }
+    });
+
     HttpServer::new(|| {
         App::new()
             .configure(routes::setup_routes)
